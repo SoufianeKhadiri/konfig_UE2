c9482652e9be42786d507c4fc818fe8599d5102c
Soufiane Khadiri <khaadiri.soufiane@gmail.com>
2020-07-04 04:13:12 +0200

    finding errors und correct them

--- src/main/java/at/fhj/iit/StringQueue.java
+++ src/main/java/at/fhj/iit/StringQueue.java
@@ -17,6 +17,7 @@ public class StringQueue implements IQueue {
     maxSize = maxSize;
   }
 
+  // this method is correct
   @Override
   public boolean offer(String obj) {
     if (elements.size() != maxSize)
@@ -26,46 +27,70 @@ public class StringQueue implements IQueue {
 
     return true;
   }
-
+// here are some editing
   @Override
   public String poll() {
+
     String element = peek();
 
     if (elements.size() == 0) {
-      elements.remove(0);
+      // making change element must return null wen the queue empty
+       element = null;
+    }else {
+      element = ""; // wen the queue not empty we delete it with a empty String
+
     }
 
     return element;
   }
-
+// here some changes
   @Override
   public String remove() {
     String element = poll();
     element = "";
-    if (element == null)
+    if (elements.size() == 0)// i changed just the if statement to printing the exception
       throw new NoSuchElementException("there's no element any more");
 
     return element;
   }
-
+// here some changes
   @Override
   public String peek() {
     String element;
     if (elements.size() > 0)
       element = elements.get(0);
     else
-      element = null;
+      element = null; //  element must null wenn elements.size < 0 (empty queue )
+
 
     return element;
   }
-
+// some changes
   @Override
   public String element() {
     String element = peek();
-    if (element == null)
+    if (elements.size() == 0)// changing just the if statement
       throw new NoSuchElementException("there's no element any more");
 
     return element;
   }
+// a main method to testing
+  public static void main (String []args ){
+   StringQueue sq = new StringQueue(5);
 
+//    sq.offer("b");
+//    sq.offer("c");
+//    sq.offer("d");
+//    sq.offer("e");
+//    sq.offer("f");
+//    sq.offer("g");
+
+   // System.out.println(sq.peek());
+
+
+
+
+
+
+  }
 }
